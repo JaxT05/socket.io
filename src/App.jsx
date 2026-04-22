7f6fbff4f9d43aa644876cf389223cc6c0b0e3ef
Jax Tobyas <153058960+JaxT05@users.noreply.github.com>
2026-04-22 18:06:27 -0400

    init commit for part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Blog } from './pages/Blog.jsx'
 import { Signup } from './pages/Signup.jsx'
 import { Login } from './pages/Login.jsx'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
-import { io } from 'socket.io-client'
+import { SocketIOContextProvider } from './contexts/SocketIOContext.jsx'
+import { Chat } from './pages/Chat.jsx'
 
 const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Blog />,
+    element: <Chat />,
   },
   {
     path: '/signup',
@@ -23,32 +23,13 @@ const router = createBrowserRouter([
   },
 ])
 
-const socket = io(import.meta.env.VITE_SOCKET_HOST, {
-  query: window.location.search.substring(1),
-  auth: {
-    token: window.localStorage.getItem('token'),
-  },
-})
-
-socket.on('connect', async () => {
-  console.log('Connected to Socket.IO as: ', socket.id)
-  const userInfo = await socket.emitWithAck('user.info', socket.id)
-  console.log('User info: ', userInfo)
-})
-
-socket.on('connect_error', (err) => {
-  console.error('Socket.IO connection error: ', err)
-})
-
-socket.on('chat.message', (msg) => {
-  console.log(`${msg.username}: ${msg.message}`)
-})
-
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <RouterProvider router={router} />
+        <SocketIOContextProvider>
+          <RouterProvider router={router} />
+        </SocketIOContextProvider>
       </AuthContextProvider>
     </QueryClientProvider>
   )
